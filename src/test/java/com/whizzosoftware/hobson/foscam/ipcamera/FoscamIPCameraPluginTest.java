9571d0fb6d5113ae3302b88fdce4aa5e6b00987c
Daniel Noguerol <dan@whizzosoftware.com>
2015-07-08 14:08:43 -0600

    Updated plugin icon.

--- src/test/java/com/whizzosoftware/hobson/foscam/ipcamera/FoscamIPCameraPluginTest.java
+++ src/test/java/com/whizzosoftware/hobson/foscam/ipcamera/FoscamIPCameraPluginTest.java
@@ -11,18 +11,24 @@ import com.whizzosoftware.foscam.camera.model.FoscamCamera;
 import com.whizzosoftware.hobson.api.device.MockDeviceManager;
 
 import com.whizzosoftware.hobson.api.property.PropertyContainer;
+import com.whizzosoftware.hobson.api.variable.MockVariableManager;
 import org.junit.Test;
+
+import java.net.InetAddress;
+
 import static org.junit.Assert.*;
 
 public class FoscamIPCameraPluginTest {
     @Test
-    public void testDiscovery() {
+    public void testDiscovery() throws Exception {
+        MockVariableManager vm = new MockVariableManager();
         MockDeviceManager dm = new MockDeviceManager();
         FoscamIPCameraPlugin plugin = new FoscamIPCameraPlugin("id", null);
+        plugin.setVariableManager(vm);
         plugin.setDeviceManager(dm);
         plugin.onStartup(new PropertyContainer());
         assertEquals(0, dm.publishedDevices.size());
-        plugin.onCameraDiscovered(new FoscamCamera("cid", "camera", null));
+        plugin.onCameraDiscovered(new FoscamCamera("cid", "camera", InetAddress.getLocalHost()));
         assertEquals(1, dm.publishedDevices.size());
     }
 }
